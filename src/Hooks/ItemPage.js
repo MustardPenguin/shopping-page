bef3e910c9b995355da6babad0f7b6bcbe076fea
MustardPenguin <andyha234@gmail.com>
2023-02-04 23:19:25 -0500

    Updated item page layout

--- src/Hooks/ItemPage.js
+++ src/Hooks/ItemPage.js
@@ -1,14 +1,47 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Items from "./Items";
+import Images from "./Images";
+import "../Styles/itempage.css";
 
 const ItemPage = () => {
+    const [amount, setAmount] = useState(1);
+
     const getItem = (id) => Items.find( (item) => item.id === id );
     const { id } = useParams();
     const item = getItem(id);
-    console.log(item);
+
+    useEffect(() => {
+
+    })
 
     return (
-        <div>Item page!</div>
+        <div className="item-page">
+            <div className="item-page-image">
+                <img src={Images[id]} alt={item.name}></img>
+            </div>
+            <div className="item-page-info">
+                <div>
+                    {item.name}
+                </div>
+                <div>
+                    ${item.price}
+                </div>
+                <div>
+                    {item.description}
+                </div>
+                <div className="quantity">
+
+                </div>
+                <div>
+                    Add to cart
+                </div>
+                <div>
+                    go back
+                </div>
+            </div>
+            
+        </div>
     )
 }
 
